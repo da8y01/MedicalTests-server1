46f733e02f50daf3911fe11e347ffc6ee1512ddf
Developer <dev@local.host>
2021-12-22 20:40:44 -0500

    Configure PostgreSQL database & Sequelize

--- app.js
+++ app.js
@@ -19,6 +19,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+const db = require("./models")
+// db.sequelize.sync()
+db.sequelize.sync({ force: true }).then(() => {
+  console.log("Drop and re-sync db.");
+})
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
