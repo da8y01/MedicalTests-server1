4bc580d35433b512b1e6c163e155273a53a9ebf8
Developer <developer@local.host>
2022-03-29 23:47:31 -0500

    Update results upload route.

--- controllers/result.controller.js
+++ controllers/result.controller.js
@@ -1,28 +1,28 @@
-const db = require('../models')
-const Result = db.results
-const Reading = db.readings
-const Op = db.Sequelize.Op
+const db = require("../models");
+const Result = db.results;
+const Reading = db.readings;
+const Op = db.Sequelize.Op;
 
 // Create and Save a new Result
 exports.create = (req, res) => {
   // Validate request
   if (!req.body.name) {
     res.status(400).send({
-      message: 'Name can not be empty!',
-    })
-    return
+      message: "Name can not be empty!",
+    });
+    return;
   }
   if (!req.body.link) {
     res.status(400).send({
-      message: 'Link can not be empty!',
-    })
-    return
+      message: "Link can not be empty!",
+    });
+    return;
   }
   if (!req.body.patient) {
     res.status(400).send({
-      message: 'Patient can not be empty!',
-    })
-    return
+      message: "Patient can not be empty!",
+    });
+    return;
   }
 
   // Create a Result
@@ -30,44 +30,45 @@ exports.create = (req, res) => {
     name: req.body.name,
     link: req.body.link,
     patient: req.body.patient,
-  }
+  };
 
   // Save Result in the database
   Result.create(result)
     .then((data) => {
-      res.send(data)
+      res.send(data);
     })
     .catch((err) => {
       res.status(500).send({
         message:
-          err.message || 'Some error occurred while creating the Result.',
-      })
-    })
-}
+          err.message || "Some error occurred while creating the Result.",
+      });
+    });
+};
 
 exports.upload = (req, res) => {
   try {
-    const file = req['file']
-    console.info('file: ', file)
+    const file = req["file"];
+    console.info("file: ", file);
     // res.statusCode = 200;
     // res.setHeader('Content-Type', 'application/json');
     // res.json(req.file);
     // res.status(200).send()
-    res.status(200).json(file)
+    res.status(200).json(file);
   } catch (error) {
-    console.error(error)
-    res.status(500).send(error.message)
+    console.info(error);
+    // res.status(500).send(error.message)
+    res.status(500).send(error);
   }
-}
+};
 
 // Retrieve all Results from the database.
 exports.findAll = (req, res) => {
   try {
-    const name = req.query.name
-    const queryPatient = req.query.patient
+    const name = req.query.name;
+    const queryPatient = req.query.patient;
     // const queryPatient = req.query.patiento
     // var condition = queryPatient ? { patient: queryPatient } : null
-    var condition = queryPatient ? { userId: queryPatient } : null
+    var condition = queryPatient ? { userId: queryPatient } : null;
     let params = {
       where: condition,
       // where: {userId: null},
@@ -76,53 +77,53 @@ exports.findAll = (req, res) => {
       include: [
         {
           model: Reading,
-          as: 'reading',
+          as: "reading",
           // where: conditionRoles,
         },
       ],
-    }
+    };
 
     Result.findAndCountAll(params)
       .then((data) => {
-        res.status(200).send(data)
+        res.status(200).send(data);
       })
       .catch((error) => {
-        console.error(error)
+        console.error(error);
         res.status(500).send({
           message:
-            error.message || 'Some error occurred while retrieving results.',
-        })
-      })
+            error.message || "Some error occurred while retrieving results.",
+        });
+      });
   } catch (error) {
-    console.error(error)
-    return res.status(500).send(error)
+    console.error(error);
+    return res.status(500).send(error);
   }
-}
+};
 
 // Find a single Result with an id
 exports.findOne = (req, res) => {
-  const id = req.params.id
+  const id = req.params.id;
 
   Result.findByPk(id)
     .then((data) => {
       if (data) {
-        res.send(data)
+        res.send(data);
       } else {
         res.status(404).send({
           message: `Cannot find Result with id=${id}.`,
-        })
+        });
       }
     })
     .catch((err) => {
       res.status(500).send({
-        message: 'Error retrieving Result with id=' + id,
-      })
-    })
-}
+        message: "Error retrieving Result with id=" + id,
+      });
+    });
+};
 
 // Update a Result by the id in the request
 exports.update = (req, res) => {
-  const id = req.params.id
+  const id = req.params.id;
 
   Result.update(req.body, {
     where: { id: id },
@@ -130,24 +131,24 @@ exports.update = (req, res) => {
     .then((num) => {
       if (num == 1) {
         res.send({
-          message: 'Result was updated successfully.',
-        })
+          message: "Result was updated successfully.",
+        });
       } else {
         res.send({
           message: `Cannot update Result with id=${id}. Maybe Result was not found or req.body is empty!`,
-        })
+        });
       }
     })
     .catch((err) => {
       res.status(500).send({
-        message: 'Error updating Result with id=' + id,
-      })
-    })
-}
+        message: "Error updating Result with id=" + id,
+      });
+    });
+};
 
 // Delete a Result with the specified id in the request
 exports.delete = (req, res) => {
-  const id = req.params.id
+  const id = req.params.id;
 
   Result.destroy({
     where: { id: id },
@@ -155,20 +156,20 @@ exports.delete = (req, res) => {
     .then((num) => {
       if (num == 1) {
         res.send({
-          message: 'Result was deleted successfully!',
-        })
+          message: "Result was deleted successfully!",
+        });
       } else {
         res.send({
           message: `Cannot delete Result with id=${id}. Maybe Result was not found!`,
-        })
+        });
       }
     })
     .catch((err) => {
       res.status(500).send({
-        message: 'Could not delete Result with id=' + id,
-      })
-    })
-}
+        message: "Could not delete Result with id=" + id,
+      });
+    });
+};
 
 // Delete all Results from the database.
 exports.deleteAll = (req, res) => {
@@ -177,25 +178,25 @@ exports.deleteAll = (req, res) => {
     truncate: false,
   })
     .then((nums) => {
-      res.send({ message: `${nums} Results were deleted successfully!` })
+      res.send({ message: `${nums} Results were deleted successfully!` });
     })
     .catch((err) => {
       res.status(500).send({
         message:
-          err.message || 'Some error occurred while removing all results.',
-      })
-    })
-}
+          err.message || "Some error occurred while removing all results.",
+      });
+    });
+};
 
 // Find all published Tutorials
 exports.findAllPublished = (req, res) => {
   Result.findAll({ where: { patient: 1 } })
     .then((data) => {
-      res.send(data)
+      res.send(data);
     })
     .catch((err) => {
       res.status(500).send({
-        message: err.message || 'Some error occurred while retrieving results.',
-      })
-    })
-}
+        message: err.message || "Some error occurred while retrieving results.",
+      });
+    });
+};
--- routes/result.routes.js
+++ routes/result.routes.js
@@ -1,23 +1,23 @@
-const multer  = require('multer')
+const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-      cb(null, 'public/');
+    cb(null, "public/");
   },
 
   filename: (req, file, cb) => {
-      cb(null, file.originalname)
-  }
+    cb(null, file.originalname);
+  },
 });
 const imageFileFilter = (req, file, cb) => {
-  if(!file.originalname.match(/\.(jpg|jpeg|png|avi|mp4|pdf)$/)) {
-      return cb(new Error('You can upload only documents files!'), false);
+  if (!file.originalname.match(/\.(jpg|jpeg|png|avi|mp4|pdf)$/)) {
+    return cb(new Error("You can upload only documents files!"), false);
   }
   cb(null, true);
 };
 
 // const upload = multer({ dest: 'public/' })
-const upload = multer({ storage: storage, fileFilter: imageFileFilter});
+const upload = multer({ storage: storage, fileFilter: imageFileFilter });
 
 module.exports = (app) => {
   const results = require("../controllers/result.controller.js");
@@ -26,12 +26,13 @@ module.exports = (app) => {
 
   // Create a new Result
   router.post("/", results.create);
-  
-  router.post("/upload", upload.single('result'), results.upload);
+
+  router.post("/upload", upload.single("exam"), results.upload);
+  // router.post("/upload", upload.array('result'), results.upload);
 
   // Retrieve all Results
   router.get("/", results.findAll);
-  
+
   // Retrieve all published Results
   router.get("/published", results.findAllPublished);
 
