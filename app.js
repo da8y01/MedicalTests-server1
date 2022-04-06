da57e9ed2e11d4f689e81bf2a069c262527f7d92
Developer <developer@local.host>
2022-04-05 22:36:05 -0500

    Force drop and resync for Heroku.

--- app.js
+++ app.js
@@ -1,67 +1,72 @@
-require('dotenv').config()
+require("dotenv").config();
 
-var createError = require('http-errors')
-var express = require('express')
-var path = require('path')
-var cookieParser = require('cookie-parser')
-var logger = require('morgan')
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index')
-var usersRouter = require('./routes/users')
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
-var app = express()
+var app = express();
 
-const cors = require('cors')
+const cors = require("cors");
 app.use(
   cors({
     // origin: 'http://localhost:4200/'
-    origin: '*',
+    origin: "*",
     // origin: ['http://localhost:4200/', 'http://localhost:8080/']
   })
-)
+);
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'jade')
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 
-const db = require('./models')
-const Role = db.role
-const User = db.user
+const db = require("./models");
+const Role = db.role;
+const User = db.user;
 
-if (process.env.NODE_ENV === 'production')
-  db.sequelize.sync().then(() => console.log('Sync db without force.')).catch(error => console.error(error))
+if (process.env.NODE_ENV === "production")
+  // db.sequelize.sync().then(() => console.log('Sync db without force.')).catch(error => console.error(error))
+  db.sequelize
+    .sync({ force: true })
+    .then(() => console.log("Drop and re-sync db."))
+    .catch((error) => console.error(error));
 else
   db.sequelize
     .sync({ force: true })
-    .then(() => console.log('Drop and re-sync db.')).catch(error => console.error(error))
+    .then(() => console.log("Drop and re-sync db."))
+    .catch((error) => console.error(error));
 
-app.use('/', indexRouter)
-app.use('/users', usersRouter)
-require('./routes/result.routes')(app)
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+require("./routes/result.routes")(app);
 // require("./routes/patient.routes")(app);
-require('./routes/auth.routes')(app)
-require('./routes/user.routes')(app)
+require("./routes/auth.routes")(app);
+require("./routes/user.routes")(app);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404))
-})
+  next(createError(404));
+});
 
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
-  res.status(err.status || 500)
-  res.render('error')
-})
+  res.status(err.status || 500);
+  res.render("error");
+});
 
-module.exports = app
+module.exports = app;
